0768d730255448a29cbaaedf41053f3be09c0207
Sebastian12689 <sebastian.serban@student.nhlstenden.com>
2023-10-13 21:38:25 +0200

    chess board in a bad way

--- data/main.py
+++ data/main.py
@@ -1,8 +1,25 @@
 import pygame
 pygame.init()
 
+gameExit = False
+
 (width, height) = (800, 800)
+white = (255, 255, 255)
+black = (0, 0, 0)
+size = 100
 
-while(True):
-    pygame.display.set_mode((width, height))
+gameDisplay = pygame.display.set_mode((width, height))
+gameDisplay.fill(white)
 
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+    for x in range(0, 8):
+        if x % 2 == 0:
+            for y in range(0, 8, 2):
+                pygame.draw.rect(gameDisplay, (0, 0, 0), (x * size, y * size, size, size))
+        else:
+            for y in range(1, 8, 2):
+                pygame.draw.rect(gameDisplay, (0, 0, 0), (x * size, y * size, size, size))
+    pygame.display.update()
